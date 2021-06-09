8f9f6d2cb5be7d29fd01ba71c5b53d6394636147
Abdullah Sahraoui <abd.sahraoui26@gmail.com>
2021-06-09 02:13:23 +0100

    Implemented logic for generating values and checking answers.

--- maths-game/src/Components/InputBox/InputBox.js
+++ maths-game/src/Components/InputBox/InputBox.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'tachyons';
 import './inputbox.css'
 import FadeIn from 'react-fade-in';
 
+const checkAnswer = (values, userValue) => {
+	switch (values.operation) {
+		case "x":
+			return(values.n1 * values.n2 === parseInt(userValue, 10)) ? true : false;
+		case "÷":
+			return(values.n1 / values.n2 === parseInt(userValue, 10)) ? true : false;
+		case "+":
+			return(values.n1 + values.n2 === parseInt(userValue, 10)) ? true : false;
+		case "-":
+			return(values.n1 - values.n2 === parseInt(userValue, 10)) ? true : false;
+		default:
+			break;
+	}
+}
 
-const InputBox = () => {
+
+const InputBox = (props) => {
+	const [userValue, setUserValue] = useState(0);
+
 	return (
 		<FadeIn className="input">
 			<div className="center pa4 form">
-				<input className="f4  pa2 w-70 center" type="number" placeholder="Enter your answer here:"/>
-				<button className="w-30 grow f4 ph3 pv2 white bg-pink shadow">Check</button>
+				<input className="f4  pa2 w-70 center" type="number" placeholder="Enter your answer here:" onChange={(e) => setUserValue(e.target.value)} />
+				<button className="w-30 grow f4 ph3 pv2 white bg-pink shadow" onClick={() => checkAnswer(props.values, userValue)}>Check</button>
 			</div>
 		</FadeIn>
 
--- maths-game/src/Components/Numbers/numbers.js
+++ maths-game/src/Components/Numbers/numbers.js
@@ -4,18 +4,32 @@ import './numbers.css';
 import FadeIn from 'react-fade-in';
 import InputBox from '../InputBox/InputBox.js';
 
+const getRndInteger = (min, max) => {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const insertNums = () => {
+	const operationsList = ["x", "÷", "+", "-"];
+
+	let n1 = getRndInteger(-9, 9);
+	let n2 = getRndInteger(-9, 9);
+	let operation = operationsList[getRndInteger(0, operationsList.length)];
+
+	return {n1, n2, operation};
+}
+
 const Numbers = () => {
 	return (
 		<FadeIn className="numbers">
 			{/*For the numbers and operations:*/}
 			<div className="tc fl w-100 pa3">
-	      <div className="fl w-40 num-one positioning">N1</div>
-	      <div className="fl w-20 operation positioning">O</div>
-	      <div className="fl w-40 num-two positioning">N2</div>
+	      <div className="fl w-40 num-one positioning"></div>
+	      <div className="fl w-20 operation positioning"></div>
+	      <div className="fl w-40 num-two positioning"></div>
 			</div>
 
 			{/*For the input box:*/}
-			<InputBox />
+			<InputBox values={insertNums()}/>
 		</FadeIn>
 	);
 }
